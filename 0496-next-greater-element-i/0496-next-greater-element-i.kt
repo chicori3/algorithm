0951a8712da87f421563d715e23d5e44044afbef
sian <chicori3@naver.com>
2025-12-09 19:36:27 +0900

    Time: 24 ms (19.1%), Space: 47 MB (51.12%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.kt
+++ 0496-next-greater-element-i/0496-next-greater-element-i.kt
@@ -1,22 +1,19 @@
 class Solution {
     fun nextGreaterElement(nums1: IntArray, nums2: IntArray): IntArray {
         val result = mutableListOf<Int>()
+        val map = mutableMapOf<Int, Int>()
+        val stack = ArrayDeque<Int>()
 
-        nums1.forEach { num ->
-            val index = nums2.indexOfFirst { it == num }
-            if (index == -1) {
-                result.add(index)
-            } else {
-                loop@ for (i in index until nums2.size) {
-                    if (nums2[i] > num) {
-                        result.add(nums2[i])
-                        break@loop
-                    }
-                    if (i + 1 == nums2.size) {
-                        result.add(-1)
-                    }
-                }
+        nums2.forEach {
+            while (stack.isNotEmpty() && stack.first() < it) {
+                map[stack.removeFirst()] = it
             }
+            
+            stack.addFirst(it)
+        }
+
+        nums1.forEach {
+            result.add(map[it] ?: -1)
         }
 
         return result.toIntArray()
