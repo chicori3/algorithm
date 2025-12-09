627dcb3abe338d82128b46b1518bc8e7b3843efe
sian <chicori3@naver.com>
2025-12-09 20:23:03 +0900

    Time: 29 ms (6.84%), Space: 47.1 MB (7.69%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun backspaceCompare(s: String, t: String): Boolean {
+        val stackA = ArrayDeque<Char>()
+        val stackB = ArrayDeque<Char>()
+        fun stacking(stack: ArrayDeque<Char>, c: Char): ArrayDeque<Char> {
+            when (c) {
+                '#' -> stack.removeFirstOrNull()
+                else -> stack.addFirst(c)
+            }
+
+            return stack
+        }
+
+        s.forEach {
+            stacking(stackA, it)
+        }
+        t.forEach {
+            stacking(stackB, it)
+        }
+
+        return stackA.joinToString { it.toString() } == stackB.joinToString { it.toString() }
+    }
+}
